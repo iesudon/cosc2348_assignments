bd5a00cef237f699d1d6dd1f1734eacec1c2c89b
iesudon <49536097+iesudon@users.noreply.github.com>
2022-02-01 18:46:59 +0000

    Create question1.py

---
+++ assignment_1/question1.py
@@ -0,0 +1,4 @@
+print("My Name is William Gordon. \n") #I always start a new line after every statement. Name line.
+print("I live on 132 Lawerence ST, 06106 , Hartford, Connecticut. \n") # Address, ZIP code, city, and state
+print("My phone number is 555-1123 \n") # Phone line
+print("My major is Computer Science, with a concentration in Cyber Security \n") # Major line
