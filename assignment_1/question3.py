cbdc00f0a6627180a82dd507ef3c8b9977d1d018
iesudon <49536097+iesudon@users.noreply.github.com>
2022-02-02 00:51:35 +0000

    Update question3.py

--- assignment_1/question3.py
+++ assignment_1/question3.py
@@ -1,5 +1,5 @@
 carSpeed = 70
 print("A car is traveling at 70 miles per hour. As a result of this: \n")
-print("The car will take 6 hours to travel ", 70*6 " miles.")
-print("The car will take 6 hours to travel ", 70*10 " miles.")
-print("The car will take 6 hours to travel ", 70*15 " miles.")
+print("The car will take 6 hours to travel ", carSpeed*6 " miles.")
+print("The car will take 10 hours to travel ", carSpeed*10 " miles.")
+print("The car will take 15 hours to travel ", carSpeed*15 " miles.")
