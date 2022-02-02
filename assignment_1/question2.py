b258638fc9d283cc84f498f79d3472dd0db74236
iesudon <49536097+iesudon@users.noreply.github.com>
2022-02-02 00:47:44 +0000

    Update question2.py

--- assignment_1/question2.py
+++ assignment_1/question2.py
@@ -1,3 +1,3 @@
-userval = int(input("Enter the amount of square feet to recieve the value in acres: "))
-result = userval/43560
-print("The entered value in acres is: ",result)
+userval = int(input("Enter the amount of square feet to recieve the value in acres: ")) #taking int input from user
+result = userval/43560 #dividing by 43560 square feet
+print("The entered value in acres is: ",result) # giving the user the end result
