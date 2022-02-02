1778bf81cf17b6f1f38227a5cc75b961012adbf2
iesudon <49536097+iesudon@users.noreply.github.com>
2022-02-02 01:38:46 +0000

    Update question3.py

--- assignment_1/question3.py
+++ assignment_1/question3.py
@@ -2,4 +2,4 @@ carSpeed = 70 # speed in miles per hour
 print ("A car is traveling at 70 miles per hour. As a result of this: \n") # beginning statement to the user
 print ("The car will take 6 hours to travel ", carSpeed * 6, " miles.") # car's distance at 6 hours
 print ("The car will take 10 hours to travel ", carSpeed * 10, " miles.") # car's distance at 10 hours
-print ("The car will take 15 hours to travel ", carSpeed * 15 " miles.") # car's distance at 15 hours
+print ("The car will take 15 hours to travel ", carSpeed * 15, " miles.") # car's distance at 15 hours
