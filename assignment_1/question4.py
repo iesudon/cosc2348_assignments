4fa9605fc93657b5b4162113408c7b3f4204ffe5
iesudon <49536097+iesudon@users.noreply.github.com>
2022-02-02 01:03:27 +0000

    Update question4.py

--- assignment_1/question4.py
+++ assignment_1/question4.py
@@ -1,9 +1,10 @@
-userVal = int(input("What is your age in years?: "))
-if(userVal <= 1):
-  print("You are an infant!")
-else if(userVal > 1 && userVal < 13):
+userVal = float(input("What is your age in years in decimal notation?: ")) # asking the user what their age is in decimal notation, 
+# and passing to if statements as float
+if(userVal <= 1): #infant statement, if they are one years old or younger
+  print("You are an infant!") 
+elif(userVal > 1 and userVal < 13): # child statement, if they are older than one and younger than 13
   print("You are a child!")
-else if(userVal >= 13 && userVal < 20):
+elif(userVal >= 13 and userVal < 20): # teenager statement, if they are at least 13 or younger than 20
   print("You are a teenager!")
-else:
+else: # if the user is older than this, the if statement will go to this
   print("You are an adult!")
