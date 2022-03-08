309ba0c4932a650852069211313bf1050dd2de05
iesudon <49536097+iesudon@users.noreply.github.com>
2022-03-08 16:13:07 -0600

    Update question4.py

--- assignment_3/question4.py
+++ assignment_3/question4.py
@@ -1,17 +1,22 @@
+import random
 class Student:
-    def __init__(self, name,courseGrades):
-        self.__courseGrades = courseGrades
-        self.__name = name
-    def find_percentage(self):
-        return sum((self.get_grades())/6)
+    def __init__(self,name,courseGrades):
+        self.courseGrades = courseGrades
+        self.name = name
+    def find_percentage(self, courseGrades):
+        tempPercentage = 0
+        for m in range(0,6):
+            tempPercentage += courseGrades[m]
+        return round(tempPercentage/6)    
     def get_name(self):
-        return self.__name
+        return self.name
+    def get_grade_at_num(self, num):
+        return self.courseGrades[num]
     def get_grades(self):
-        return self.__courseGrades
+        return self.courseGrades
 
 def selectionSort(arr, names ,size):
-    i = 0
-    for i in range(0, size-1): ## for int i in range 0, till size-1
+    for i in range(0, size): ## for int i in range 0, till size-1
         unsortedMin = i ## the min value of the list is i
         j = i + 1 # j, another variable is i + 1
         while (j < size): # while  j is less than size
@@ -21,7 +26,7 @@ def selectionSort(arr, names ,size):
         temp = arr[i] # temporary variable is equal to arr at i
         tempNames = names[i] 
         arr[i] = arr[unsortedMin] ## arr at i is equal to arr at unsortedMin
-        names[i] = tempNames
+        names[i] = names[unsortedMin]
         arr[unsortedMin] = temp ## arr at unsorted min is equal to temp
         names[unsortedMin] = tempNames
 def main():
@@ -29,37 +34,32 @@ def main():
     numCourses = 6
     
     studentClasses = []
-    courseGrades = []
+    courseGrades_temp = []
     
     for i in range(0,numStudents):
         inputStudentName = input("Enter the name of the student: ")
-        for j in range(0,numCourses):
-            inputCourseGrade = int(input("Enter the course grade for course "+ str(j+1) + ": "))
-            while(inputCourseGrade > 100 or inputCourseGrade < 0):
-                print("Enter a valid grade: ")
-                inputCourseGrade = int(input("Enter the course grade for course "+ str(j+1) + ": "))
-            courseGrades.append(inputCourseGrade)
-        studentClasses.append(Student(inputStudentName,courseGrades))
-
-    print("\n")
-    print(studentClasses[0].get_grades())
-    print("\n")
-    sortedPercentageList = []
+        courseGrades_temp = [ random.randint(10,100) for i in range(0,6) ]
+        print(courseGrades_temp)
+        studentClasses.append(Student(inputStudentName,courseGrades_temp))
+    
+    sortedGrades = []
     sortedNames = []
-
-    for m in range(0,numStudents):
-        tempPercentage  = studentClasses[m].find_percentage()
-        sortedPercentageList.append(tempPercentage)
-        print(studentClasses[m].find_percentage())
-        sortedNames.append(studentClasses[m].get_name())
-
-
-    print(sortedPercentageList)
-    selectionSort(sortedPercentageList, sortedNames, numStudents)
-    print(sortedPercentageList)
-
-    for k in range(0,numStudents):
-            print("Student " + studentClasses[k].get_name() + " percentage average is " + str(sortedPercentageList[k]))
+    for temp in range(0,numStudents):
+        sortedGrades.append((studentClasses[temp].find_percentage(studentClasses[temp].get_grades())))
+        sortedNames.append(studentClasses[temp].get_name())
+        
+    selectionSort(sortedGrades,sortedNames,numStudents)
+    print("Average Grades of all courses " + str(sortedGrades))
+    print("Names of the students "+str(sortedNames))
+    
+    courseAverages = []
+    count = 0
+    while count != 6:
+        avg = 0
+        for i in range(0,numStudents):
+            avg += studentClasses[i].get_grade_at_num(count)
+        courseAverages.append(round((avg/numStudents),2))
+        count += 1
+    print("\n")
+    print("Average course grades "+str(courseAverages))
 main()
-
-
