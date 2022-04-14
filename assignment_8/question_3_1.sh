c2dd07eaa19a6d462ad19ac8a7c5b55cc6116bc3
iesudon <49536097+iesudon@users.noreply.github.com>
2022-04-14 11:26:14 -0500

    Update question_3_1.sh

--- assignment_8/question_3_1.sh
+++ assignment_8/question_3_1.sh
@@ -4,16 +4,16 @@ goal=50
 start=2
 for i in $(seq $start $goal)
 do
-    k=0
+    count=0
     for j in $(seq 2 $(($i - 1)))
     do 
         if [ $(($i % $j)) -eq 0 ]
         then
-            k=1
+            count=1
             break
         fi
     done
-    if [ $k -eq 0 ]
+    if [ $count -eq 0 ]
     then
     echo $i
     fi
